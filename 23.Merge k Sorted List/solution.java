5fff4ac0106eed7ae3bb0a2debf282776eb38da5
WangHan Lee <lee.wanghan@gmail.com>
2025-08-06 18:01:08 +0800

    upload q19 q23 and q143

---
+++ 23.Merge k Sorted List/solution.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+
+        for (int i = 1; i < lists.length; i++) {
+            lists[i] = merge(lists[i], lists[i - 1]);
+        }
+        return lists[lists.length - 1];
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
